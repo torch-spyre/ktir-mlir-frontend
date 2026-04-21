e3afd26c408700406997926a372ac519dbe31636
Bardia Mahjour <bmahjour@ca.ibm.com>
2026-04-21 11:37:32 -0400

    add linker flags to avoid discarding symbols that are only referenced in MLIR's static initializers + only register dependent dialects (not all)
    Signed-off-by: Bardia Mahjour <bmahjour@ca.ibm.com>

--- tools/ktir-opt/ktir-opt.cpp
+++ tools/ktir-opt/ktir-opt.cpp
@@ -2,19 +2,30 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "mlir/IR/Dialect.h"
-#include "mlir/InitAllDialects.h"
-#include "mlir/InitAllPasses.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
-
+#include "mlir/IR/Dialect.h"
+#include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/Linalg/IR/Linalg.h"
+#include "mlir/Dialect/Math/IR/Math.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "Ktdp/KtdpDialect.hpp"
 
 int main(int argc, char **argv) {
-  mlir::registerAllPasses();
-
   mlir::DialectRegistry registry;
-  registry.insert<mlir::ktdp::KtdpDialect>();
-  mlir::registerAllDialects(registry);
+
+  registry.insert<mlir::affine::AffineDialect,
+                  mlir::arith::ArithDialect,
+                  mlir::func::FuncDialect,
+                  mlir::linalg::LinalgDialect,
+                  mlir::math::MathDialect,
+                  mlir::memref::MemRefDialect,
+                  mlir::scf::SCFDialect,
+                  mlir::tensor::TensorDialect,
+                  mlir::ktdp::KtdpDialect>();
 
   return mlir::asMainReturnCode(
       mlir::MlirOptMain(argc, argv, "KTIR optimizer driver\n", registry));
